177fd28e6945f1ef7b80c29bede7c655115b2423
Akshay kishore <akshaykishore02@outlook.com>
2024-09-05 23:33:21 +0530

    Add project files.

---
+++ FormApplication/Utility/Connection.cs
@@ -0,0 +1,8 @@
+﻿public static class Connection
+{
+    private static string Name = "data source=AKSHAY; Initial Catalog=MyProjectDB; User Id=MyProjectDB;Password=Akshay@2002; TrustServerCertificate=True; Trusted_Connection=SSPI; Encrypt=false;";
+    public static string CName
+    {
+        get => Name;
+    }
+} 
